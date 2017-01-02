dc62420a0915c45a5b7d00af598d285e0e1865ec
PivkoDev <bartosz.piwkowski@gmail.com>
2017-01-02 18:40:41 +0100

    Externals and OpenGL

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,31 @@
+#include "glew.h"
+#include "freeglut.h"
+#include <iostream>
 
+void renderScene(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.1, 0.2, 0.1, 1.0);//clear red
+
+	glutSwapBuffers();
+}
 
 int main(int argc, char **argv)
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);//optional
+	glutInitWindowSize(800, 600); //optional
+
+	glutCreateWindow("OpenGL First Window");
+
+	glEnable(GL_DEPTH_TEST);
+
+	// register callbacks
+	glutDisplayFunc(renderScene);
+
+	glutMainLoop();
+
 	return 0;
 }
